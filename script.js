cb65b0d2d6bbac876d87ffddbf219cbf4c14af94
Anshul <avenger720720@gmail.com>
2025-01-15 18:22:19 +0530

    completedd

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ rect.addEventListener('mousemove', (details) => {
    let insiderectval =  details.clientX - rectanglelocation.left;
     
    if(insiderectval < rectanglelocation.width/2){
-    console.log("left")
+        let redColor = gsap.utils.mapRange(0, rectanglelocation.width/2, 255, 0, insiderectval)
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redColor}, 0, 0)`,
+            ease: Power4,
+        })
    }
    else{
-    console.log("right");
+    let blueColor = gsap.utils.mapRange(rectanglelocation.width/2, rectanglelocation.width,0,255,insiderectval)
+    gsap.to(rect, {
+        backgroundColor: `rgb(0, 0, ${blueColor})`,
+        ease: Power4,
+    })
     
    }
     
+})
+
+rect.addEventListener('mouseleave', () => {
+    gsap.to(rect, {
+        backgroundColor: "white"
+    })
 })
